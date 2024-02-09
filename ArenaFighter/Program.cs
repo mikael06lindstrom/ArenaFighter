5e8e9c7c3c36eb4e191af561f15e6c8bf40fd293
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2024-02-09 14:56:39 +0100

    Add project files.

---
+++ ArenaFighter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArenaFighter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
