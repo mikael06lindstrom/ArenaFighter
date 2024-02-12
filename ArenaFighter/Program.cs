e6ef8cfb5ede33e27866d9e766e230697d808879
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2024-02-12 12:45:55 +0100

    Start new battle

--- ArenaFighter/Program.cs
+++ ArenaFighter/Program.cs
@@ -23,11 +23,36 @@ namespace ArenaFighter
         /// <param name="args">Anything</param>
         static void Main(string[] args)
         {
+            // Names for character in this battle game
+            string[] optName = new string[6] { "Erik", "Valter", "David", "Viktor", "Gustav", "Ulrik" };
+            string name;
+
             // Welcome the user to this program
             Console.WriteLine($"Welcome to {ProgramName}!");
 
             // Make a pause in this program
             Console.ReadKey();
+
+            // Ask the user for a name of their character
+            Console.Write("Write a name for your character: ");
+            name = Console.ReadLine();
+
+            // Create instans of character class for the player character
+            Character player = new Character(name);
+
+            // Run until the player not alive or the player had choise to retired
+            do
+            {
+                // Create a opponent character to the player
+                Character opponent = new Character(optName[random.Next(0, optName.Length)]);
+
+                // Create a new battle
+                Battle battle = new Battle(player, opponent);
+
+                // Make a pause in the game
+                Console.ReadKey();
+
+            } while (player.IsAlived == true && player.IsRetired == false);
         }
     }
 }
